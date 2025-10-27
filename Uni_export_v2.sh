c0bb3acaf7835d2becf7757150d90f6837968fba
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-10-27 10:56:08 +0500

    Update Uni_export_v2.sh

--- Uni_export_v2.sh
+++ Uni_export_v2.sh
@@ -36,11 +36,11 @@ check_hq_config() {
         "ntp timezone utc+5"
         "interface tunnel.0"
         "ip address 172.16.0.1/30"
-        "ip tunnel 172.16.1.2 172.16.2.2 mode gre"
+        "ip tunnel 172.16.1.4 172.16.2.5 mode gre"
         "ip ospf authentication-key 0x8de456332b943f87"
         "interface int0"
         "ip nat outside"
-        "ip address 172.16.1.2/28"
+        "ip address 172.16.1.4/28"
         "interface int1"
         "ip nat inside"
         "ip address 192.168.1.1/27"
@@ -54,7 +54,6 @@ check_hq_config() {
         "encapsulation dot1q 200"
         "encapsulation dot1q 100"
         "ip nat pool"
-        "ip nat source dynamic inside-to-outside pool np overload interface int0"
     )
     
     # Проверяем каждую ожидаемую строку
@@ -143,16 +142,15 @@ check_br_config() {
         "ntp timezone utc+5"
         "interface tunnel.0"
         "ip address 172.16.0.2/30"
-        "ip tunnel 172.16.2.2 172.16.1.2 mode gre"
+        "ip tunnel 172.16.2.5 172.16.1.4 mode gre"
         "ip ospf authentication-key 0x8de456332b943f87"
         "interface int0"
         "ip nat outside"
-        "ip address 172.16.2.2/28"
+        "ip address 172.16.2.4/28"
         "interface int1"
         "ip nat inside"
         "ip address 192.168.3.1/28"
         "ip nat pool"
-        "ip nat source dynamic inside-to-outside pool np overload interface int0"
     )
     
     # Проверяем каждую ожидаемую строку
