83f247e1429e163e7f0d9fe09d4e60808e3fd9b9
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-12-25 15:38:02 +0500

    Update ISP-Module-2.sh

--- ISP-Module-2.sh
+++ ISP-Module-2.sh
@@ -39,10 +39,10 @@ log_and_echo "=========================================="
 log_and_echo "Критерий 1"
 run_check "Проверка сетевой службы NTP" "chronyc clients | grep -e '172.16.1.4' -e '172.16.2.5'"
 if command -v curl > /dev/null; then
-run_check "Проверка работоспособности сайта на HQ-SRV" "curl -I http://172.16.1.1"
-run_check "Проверка работоспособности сайта на BR-SRV" "curl -I http://172.16.2.1"
+run_check "Проверка работоспособности сайта на HQ-SRV" "timeout 10 curl -I http://172.16.1.1"
+run_check "Проверка работоспособности сайта на BR-SRV" "timeout 10 curl -I http://172.16.2.1"
 else
 apt-get update && apt-get install curl -y
-run_check "Проверка работоспособности сайта на HQ-SRV" "curl -I http://172.16.1.1"
-run_check "Проверка работоспособности сайта на BR-SRV" "curl -I http://172.16.2.1"
+run_check "Проверка работоспособности сайта на HQ-SRV" "timeout 10 curl -I http://172.16.1.1"
+run_check "Проверка работоспособности сайта на BR-SRV" "timeout 10 curl -I http://172.16.2.1"
 fi
